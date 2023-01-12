98c389b6c6dd9360b667d439a7eb13008cc4c3b3
shreyas <shreyas@tcs.com>
2023-01-12 07:09:58 +0000

    Second Project Java Code

---
+++ Day01/second-java-app/src/main/java/com/shreyas/app/Greeting.java
@@ -0,0 +1,18 @@
+package com.shreyas.app;
+
+public class Greeting{
+
+    public String SayWelcome(){
+        return "welcome to greeting";
+    }
+
+    public String SayHello(){
+        return "hello from greeting";
+    }
+
+    public String SayBye(){
+        return "bye from greeting";
+    }
+
+
+}
