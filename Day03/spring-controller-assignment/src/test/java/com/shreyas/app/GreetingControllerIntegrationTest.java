40710dde6d8c36b9b2759a884ad0ceecfea7ef3e
shreyas <shreyas@tcs.com>
2023-01-16 14:48:50 +0000

    Day3 Assignment of SpringControllerAssignmentApplication

--- Day03/spring-controller-assignment/src/test/java/com/shreyas/app/GreetingControllerIntegrationTest.java
+++ Day03/spring-controller-assignment/src/test/java/com/shreyas/app/GreetingControllerIntegrationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
