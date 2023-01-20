ae77e0fd0c3d19f767ebc6a52f2de502c1d1f00b
sfjbs <shrysh456@gmail.com>
2023-01-20 07:35:05 +0000

    Day 6 Patient App

--- Day06/patient-app/src/main/java/com/shreyas/com/config/PatientSecurity.java
+++ Day06/patient-app/src/main/java/com/shreyas/com/config/PatientSecurity.java
@@ -3,15 +3,19 @@ package com.shreyas.com.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+
+
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @Configuration
-public class PatientSecurity extends WebSecurityConfigurerAdapter {
+public class PatientSecurity extends WebSecurityConfigurerAdapter{
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.httpBasic().and().authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/patient/save").hasAnyRole("USER")
                 .antMatchers(HttpMethod.PUT, "/patient/update").hasAnyRole("ADMIN").and().csrf().disable().headers()
@@ -20,7 +24,7 @@ public class PatientSecurity extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("shreyas").password("{noop}password").roles("USER").and()
-                .withUser("darkprince").password("{noop}password").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("shreyaspatil").password("{noop}12345").roles("USER").and()
+                .withUser("shreyas").password("{noop}67890").roles("ADMIN");
     }
 }
